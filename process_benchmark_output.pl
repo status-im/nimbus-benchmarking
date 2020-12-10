8b767d8b6d81e14dacc4acdef1c04685067ebf04
Ștefan Talpalaru <stefantalpalaru@yahoo.com>
2020-12-10 21:52:50 +0100

    produce HTML charts using a d3.js wrapper lib

--- process_benchmark_output.pl
+++ process_benchmark_output.pl
@@ -8,26 +8,45 @@
 # according to those terms.
 
 use strict;
-use warnings;
+use warnings 'all';
 use Getopt::Long;
 use Pod::Usage;
 use JSON::PP;
+use Time::Piece;
+
+# Welcome to the world of insanely fast and unreadable code!
+#
+# By the time you're done reading this file, you're guaranteed to stop worrying
+# and love references, sigils, streamlined regex operators and dynamic typing.
+#
+# Hey, did you know Perl hashes can only have scalar values? Also, you'll never
+# guess what inspired PHP...
 
 my $help = 0;
 my $bench_type = '';
+my $output_type = 'jenkins';
 my $input_file = '';
 my $output_file = '';
-my %output;
-$output{'groups'} = ();
+my $output_dir = '';
+my %output_jenkins;
+$output_jenkins{'groups'} = ();
+my %output_d3;
+my $timestr = gmtime()->strftime('%Y-%m-%d-%H:%M:%S');
 
+# Surprisingly elegant option parsing.
+# The help text is at the bottom of the file, though, to keep things weird.
 GetOptions('help' => \$help,
     'type=s' => \$bench_type,
+    'output-type=s' => \$output_type,
     'infile=s' => \$input_file,
-    'outfile=s' => \$output_file) or pod2usage(-verbose => 1, -exitval => 1);
+    'outfile=s' => \$output_file,
+    'outdir=s' => \$output_dir) or pod2usage(-verbose => 1, -exitval => 1);
 pod2usage(-verbose => 1, -exitval => 0) if $help;
 pod2usage(-msg => 'Missing a benchmark type', -exitval => 1) if $bench_type eq '';
+pod2usage(-msg => 'Missing an output type', -exitval => 1) if ($output_type ne 'jenkins' && $output_type ne 'd3');
 pod2usage(-msg => 'Missing an input file', -exitval => 1) if $input_file eq '';
-pod2usage(-msg => 'Missing an output file', -exitval => 1) if $output_file eq '';
+pod2usage(-msg => 'Missing an output file', -exitval => 1) if ($output_type eq 'jenkins' && $output_file eq '');
+pod2usage(-msg => 'Missing an output dir', -exitval => 1) if ($output_type eq 'd3' && $output_dir eq '');
 
 my %group = (
     name => $bench_type,
@@ -36,12 +55,14 @@ $group{'tests'} = ();
 
 open(my $input_handle, '<', $input_file) or die "Unable to open file '$input_file': $!";
 
+# Different benchmark groups have different output formats we need to parse.
 if ($bench_type eq 'block_sim') {
     my $start_processing = 0;
     my $validators = 0;
     my $epoch_length = 0;
     while (<$input_handle>) {
-        # strip both ends
+        # Strip both ends. Aren't hidden loop variables and default operator
+        # arguments great?
         s/^\s+|\s+$//;
 
         $start_processing = 1 if /^Done!$/;
@@ -51,9 +72,13 @@ if ($bench_type eq 'block_sim') {
             ($validators, $epoch_length) = ($1, $2);
         } elsif (/^(\d+\.\d+),\s+(\d+\.\d+),\s+(\d+\.\d+),\s+(\d+\.\d+),\s+(\d+), (.+)$/) {
             my ($average, $stddev, $min, $max, $samples, $test_name) = ($1, $2, $3, $4, $5, $6);
+            # TODO: extract the common parsing part in a subroutine.
+
+            # Jenkins
             my %test = (
                 name => $test_name,
             );
+            # This particular line noise works. That's all you need to know.
             @{$test{'parameters'}} = (
                 {
                     name => 'validators',
@@ -70,7 +95,14 @@ if ($bench_type eq 'block_sim') {
                     value => $average * 1,
                 },
             );
+            # Hash references are a great way to confuse readers.
             push(@{$group{'tests'}}, \%test);
+            # D3
+            # Guess how we converts strings to floats!
+            $output_d3{$test_name} = {
+                average => $average * 1,
+                stddev => $stddev * 1,
+            };
         }
     }
 } elsif ($bench_type eq 'state_sim') {
@@ -78,7 +110,7 @@ if ($bench_type eq 'block_sim') {
     my $validators = 0;
     my $epoch_length = 0;
     while (<$input_handle>) {
-        # strip both ends
+        # Strip both ends.
         s/^\s+|\s+$//;
 
         $start_processing = 1 if /^:Done!$/;
@@ -88,6 +120,7 @@ if ($bench_type eq 'block_sim') {
             ($validators, $epoch_length) = ($1, $2);
         } elsif (/^(\d+\.\d+),\s+(\d+\.\d+),\s+(\d+\.\d+),\s+(\d+\.\d+),\s+(\d+), (.+)$/) {
             my ($average, $stddev, $min, $max, $samples, $test_name) = ($1, $2, $3, $4, $5, $6);
+            # Jenkins
             my %test = (
                 name => $test_name,
             );
@@ -108,18 +141,54 @@ if ($bench_type eq 'block_sim') {
                 },
             );
             push(@{$group{'tests'}}, \%test);
+            # D3
+            $output_d3{$test_name} = {
+                average => $average * 1,
+                stddev => $stddev * 1,
+            };
         }
     }
 } else {
     die "Unknown benchmark type: $bench_type";
 }
 
-push (@{$output{'groups'}}, \%group);
+push (@{$output_jenkins{'groups'}}, \%group);
 
-open(my $output_handle, '>', $output_file) or die "Unable to open file '$output_file': $!";
-my $json_output = JSON::PP->new->utf8->pretty->encode(\%output);
-print $output_handle $json_output;
-close($output_handle);
+# We don't want to depend on modules not shipped by default.
+my $json = JSON::PP->new->utf8;
+if ($output_type eq 'jenkins') {
+    open(my $output_handle, '>', $output_file) or die "Unable to open file '$output_file': $!";
+    print $output_handle $json->pretty->encode(\%output_jenkins);
+    close($output_handle);
+} elsif ($output_type eq 'd3') {
+    my @benchmarks;
+    $output_file = $output_dir . '/' . $bench_type . '_d3.js';
+
+    if (-e $output_file) {
+        # Append the new data to an existing file. Who needs backups?
+        open(my $output_handle, '<:encoding(UTF-8)', $output_file) or die "Unable to open file '$output_file': $!";
+        my $old_benchmarks_text;
+        # Ugly trick to read the entire file content in one go.
+        { local $/ = undef; $old_benchmarks_text = <$output_handle>; }
+        # This JS head and tail should probably be moved to variables.
+        # We're never going to have unquoted single quotes inside our JSON data, right?
+        $old_benchmarks_text =~ s/^var jsonData = '(.*)';$/$1/;
+        # Let the users handle the errors, since it's probably their fault anyway.
+        @benchmarks = @{$json->decode($old_benchmarks_text)};
+        close($output_handle);
+    }
+
+    # Don't worry, we'll process this further in JavaScript.
+    push(@benchmarks, {
+            'timestamp' => $timestr,
+            'data' => \%output_d3,
+    });
+
+    open(my $output_handle, '>', $output_file) or die "Unable to open file '$output_file': $!";
+    # No pretty printing here. We want one big JSON string to shove into a JS var.
+    print $output_handle "var jsonData = '" . $json->encode(\@benchmarks) . "';";
+    close($output_handle);
+}
 
 
 __END__
@@ -132,7 +201,13 @@ process_benchmark_output.pl
 
 =head1 SYNOPSIS
 
+For a Jenkins benchmark plugin:
+
  process_benchmark_output.pl --type block_sim --infile block_sim_out.txt --outfile results/block_sim/result.json
+
+For a "d3.js" visualisation:
+
+ process_benchmark_output.pl --type block_sim --output-type d3 --infile block_sim_out.txt --outdir benchmark_results
 
 or
 
--- run_nbc_benchmarks.sh
+++ run_nbc_benchmarks.sh
@@ -9,7 +9,75 @@
 
 set -e
 
-[[ -z "$NPROC" ]] && NPROC=2 # number of CPU cores available
+# OS detection
+if uname | grep -qi darwin; then
+	# macOS
+	MAX_NPROC="$(sysctl -n hw.logicalcpu)"
+	GETOPT_BINARY="/usr/local/opt/gnu-getopt/bin/getopt"
+	[[ -f "$GETOPT_BINARY" ]] || { echo "GNU getopt not installed. Please run 'brew install gnu-getopt'. Aborting."; exit 1; }
+else
+	MAX_NPROC="$(nproc)"
+	GETOPT_BINARY="getopt"
+fi
+
+# argument parsing
+! ${GETOPT_BINARY} --test > /dev/null
+if [ ${PIPESTATUS[0]} != 4 ]; then
+  echo '`getopt --test` failed in this environment.'
+  exit 1
+fi
+
+OPTS="h"
+LONGOPTS="help,jobs:,output-type:"
+
+# default values
+NPROC="${MAX_NPROC}"
+OUTPUT_TYPE="jenkins"
+
+print_help() {
+  cat <<EOF
+Usage: $(basename "$0") --jobs <number of parallel build jobs> --output-type <jenkins|d3>
+
+  -h, --help                  this help message
+  --jobs                      number of parallel build jobs (default: ${NPROC})
+  --output-type               output type ("jenkins" or "d3"; default: ${OUTPUT_TYPE})
+EOF
+}
+
+! PARSED=$(${GETOPT_BINARY} --options=${OPTS} --longoptions=${LONGOPTS} --name "$0" -- "$@")
+if [ ${PIPESTATUS[0]} != 0 ]; then
+  # getopt has complained about wrong arguments to stdout
+  exit 1
+fi
+
+# read getopt's output this way to handle the quoting right
+eval set -- "$PARSED"
+while true; do
+  case "$1" in
+    -h|--help)
+      print_help
+      exit
+      ;;
+    --jobs)
+      NPROC="$2"
+      shift 2
+      ;;
+    --output-type)
+      OUTPUT_TYPE="$2"
+      shift 2
+      ;;
+    --)
+      shift
+      break
+      ;;
+    *)
+      echo "argument parsing error"
+      print_help
+      exit 1
+  esac
+done
+
+# benchmarking
 
 REL_PATH="$(dirname ${BASH_SOURCE[0]})"
 
@@ -21,11 +89,43 @@ if [[ -f "research/state_sim.nim" ]]; then
 	BENCHMARKS+=( state_sim )
 fi
 
-make -j${NPROC} ${BENCHMARKS[*]}
+make -j${NPROC} NIMFLAGS="-f" ${BENCHMARKS[*]}
+
+if [[ ${OUTPUT_TYPE} == "jenkins" ]]; then
+	OUT_DIR="."
+	MSG=( "\nJenkins benchmark results generated." )
+elif [[ ${OUTPUT_TYPE} == "d3" ]]; then
+	OUT_DIR="benchmark_results"
+	MSG=( "\nYou can open the following URLs in your browser:" )
+fi
 
 for BENCH in ${BENCHMARKS[*]}; do
-	build/${BENCH} > ${BENCH}_out.txt
-	mkdir -p results/${BENCH}
-	${REL_PATH}/process_benchmark_output.pl --type ${BENCH} --infile ${BENCH}_out.txt --outfile results/${BENCH}/result.json
+	echo -n "Running ${BENCH} benchmarks..."
+	build/${BENCH} > "${OUT_DIR}/${BENCH}_out.txt"
+	echo
+	# process the benchmark results
+	if [[ ${OUTPUT_TYPE} == "jenkins" ]]; then
+		mkdir -p results/${BENCH}
+		${REL_PATH}/process_benchmark_output.pl \
+			--type ${BENCH} \
+			--infile "${OUT_DIR}/${BENCH}_out.txt" \
+			--output-type "${OUTPUT_TYPE}" \
+			--outfile results/${BENCH}/result.json
+	elif [[ ${OUTPUT_TYPE} == "d3" ]]; then
+		mkdir -p "${OUT_DIR}"
+		${REL_PATH}/process_benchmark_output.pl \
+			--type ${BENCH} \
+			--infile "${OUT_DIR}/${BENCH}_out.txt" \
+			--output-type "${OUTPUT_TYPE}" \
+			--outdir "${OUT_DIR}"
+		sed \
+			-e "s/%BENCH_NAME%/${BENCH}/g" \
+			${REL_PATH}/template.html > "${OUT_DIR}/${BENCH}.html"
+		MSG+=( "file://$(pwd)/${OUT_DIR}/${BENCH}.html" )
+	fi
+done
+
+for LINE in "${MSG[@]}"; do
+	echo -e "${LINE}"
 done
 
